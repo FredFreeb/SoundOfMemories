6c18358ccb7f47d6ca163c3c6b23f30b48de4d99
FredFribel <frederic.fribel@hotmail.fr>
2023-04-09 20:30:05 +0200

    page  you tube centré

--- js/yT.js
+++ js/yT.js
@@ -37,3 +37,26 @@ function onPlayerStateChange(event) {
 function stopVideo() {
     player.stopVideo();
 }
+$(document).ready(function(){
+    $('.slick-carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        responsive: [
+    {
+        breakpoint: 768,
+        settings: {
+        slidesToShow: 2
+        }
+    },
+    {
+        breakpoint: 480,
+        settings: {
+        slidesToShow: 1
+        }
+    }
+    ]
+});
+});
