d2f95820636c9d6d4294cfe9dc7bdaff3cd57e2f
FredFribel <frederic.fribel@hotmail.fr>
2023-04-10 11:29:23 +0200

    youtube api n'apparait pas et minify jpg a modifier

--- js/yT.js
+++ js/yT.js
@@ -1,62 +1,18 @@
-// This code loads the IFrame Player API code asynchronously.
-let tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-let firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-// This function creates an <iframe> (and YouTube player)
-// after the API code downloads.
-let idVideo = "HnPMrJfOINo";
-let player;
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        height: '390',
-        width: '640',
-        videoId: idVideo,
-        events: {
-//            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        },
-        playerVars: {
-            'enablejsapi': 1,
-            'controls': 1,
-            'disablekb': 1
+let nextPageToken =""
+function getVideos(){
+    fetch('https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UC8tCjXrtfNBZS2f6ZGnCbEQ&maxResults=10&order=date&key=AIzaSyBepb7bGe7yguPiVQuHvXJh_oh6gm6pO8c&pageToken='+nextPageToken)
+    .then((result)=>{
+        return result.json()
+    }).then((data)=> {
+        console.log(data)
+        let videos = data.items
+        nextPageToken = data.nextPageToken
+        let videoContainer = document.querySelector(".youtube-container")
+        for(video of videos){
+            videoContainer.innerHTML += `
+            <h3>"${video.snippet.title}"</h3>
+            <img src="${video.snippet.thumbnails.high.url}">
+                `
         }
-    });
+    })
 }
-
-function onPlayerReady(event) {
-    event.target.playVideo();
-}
-let done = false;
-function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 6000);
-    done = true;
-}
-}
-function stopVideo() {
-    player.stopVideo();
-}
-$(document).ready(function(){
-    $('.slick-carousel').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        dots: true,
-        responsive: [
-    {
-        breakpoint: 768,
-        settings: {
-        slidesToShow: 2
-        }
-    },
-    {
-        breakpoint: 480,
-        settings: {
-        slidesToShow: 1
-        }
-    }
-    ]
-});
-});
